7102cabe60b51255a57ddb2e5a1aafbdf6154937
Mikhail Krasikov <mikhail_krasikov@epam.com>
2022-05-02 19:23:24 +0300

    v0 - Initial commit

---
+++ src/main/java/com/mkrasikoff/figmacomparator/FigmaComparatorApplication.java
@@ -0,0 +1,13 @@
+package com.mkrasikoff.figmacomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigmaComparatorApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(FigmaComparatorApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/mkrasikoff/figmacomparator/FigmaComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkrasikoff.figmacomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FigmaComparatorApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
