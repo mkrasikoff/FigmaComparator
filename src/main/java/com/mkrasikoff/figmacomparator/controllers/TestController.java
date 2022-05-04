ae8d97c832e62ec1eec1fd6568d5977e907b3d38
Mikhail Krasikov <mikhail_krasikov@epam.com>
2022-05-04 21:42:04 +0300

    v2 - Try to do html input form

--- src/main/java/com/mkrasikoff/figmacomparator/controllers/TestController.java
+++ src/main/java/com/mkrasikoff/figmacomparator/controllers/TestController.java
@@ -1,15 +1,28 @@
 package com.mkrasikoff.figmacomparator.controllers;
 
+import com.mkrasikoff.figmacomparator.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("test")
+@RequestMapping("")
 public class TestController {
 
-   @GetMapping("")
-   public String test() {
-      return "Hello message!";
+   @GetMapping("/test")
+   public ModelAndView test() {
+      ModelAndView modelAndView = new ModelAndView();
+      modelAndView.setViewName("test.html");
+      return modelAndView;
+   }
+
+   @PostMapping("/submit")
+   public String getTokenFromUser(@ModelAttribute("user") User user) {
+
+      System.out.println("Current token: " + user.getToken());
+      return "redirect:test.html";
    }
 }
---
+++ src/main/java/com/mkrasikoff/figmacomparator/models/User.java
@@ -0,0 +1,9 @@
+package com.mkrasikoff.figmacomparator.models;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+   private String token;
+}
