23c009257d993ccf79a5d8309db65fab2fbcdbcb
Mikhail Krasikov <mikhail_krasikov@epam.com>
2022-05-05 16:00:56 +0300

    v3 - Proceed temporary token save system to UserDao

--- src/main/java/com/mkrasikoff/figmacomparator/controllers/TestController.java
+++ src/main/java/com/mkrasikoff/figmacomparator/controllers/TestController.java
@@ -1,28 +1,35 @@
 package com.mkrasikoff.figmacomparator.controllers;
 
+import com.mkrasikoff.figmacomparator.daos.UserDao;
 import com.mkrasikoff.figmacomparator.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/login")
 public class TestController {
 
-   @GetMapping("/test")
+   @Autowired
+   private UserDao userDao;
+
+   @GetMapping("/token")
    public ModelAndView test() {
       ModelAndView modelAndView = new ModelAndView();
-      modelAndView.setViewName("test.html");
+      modelAndView.addObject("user", new User());
+      modelAndView.setViewName("loginPage");
       return modelAndView;
    }
 
-   @PostMapping("/submit")
-   public String getTokenFromUser(@ModelAttribute("user") User user) {
+   @PostMapping("/token")
+   public RedirectView getTokenFromUser(@ModelAttribute("user") User user) {
 
-      System.out.println("Current token: " + user.getToken());
-      return "redirect:test.html";
+      userDao.saveActualToken(user);
+      return new RedirectView("/login/token");
    }
 }
