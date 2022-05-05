9e126d2f08c582d0d6491075249868803d561483
Mikhail Krasikov <mikhail_krasikov@epam.com>
2022-05-05 17:51:32 +0300

    v4 - Do validation and progress with new page style

--- src/main/java/com/mkrasikoff/figmacomparator/controllers/TemporaryLoginController.java
+++ src/main/java/com/mkrasikoff/figmacomparator/controllers/TemporaryLoginController.java
@@ -3,33 +3,54 @@ package com.mkrasikoff.figmacomparator.controllers;
 import com.mkrasikoff.figmacomparator.daos.UserDao;
 import com.mkrasikoff.figmacomparator.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/login")
-public class TestController {
+public class TemporaryLoginController {
 
    @Autowired
    private UserDao userDao;
 
    @GetMapping("/token")
    public ModelAndView test() {
+
       ModelAndView modelAndView = new ModelAndView();
       modelAndView.addObject("user", new User());
       modelAndView.setViewName("loginPage");
+
       return modelAndView;
    }
 
    @PostMapping("/token")
-   public RedirectView getTokenFromUser(@ModelAttribute("user") User user) {
+   public ModelAndView getTokenFromUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+
+      ModelAndView modelAndView = new ModelAndView();
+
+      if(bindingResult.hasErrors()) {
+         modelAndView.setViewName("loginPage");
+         return modelAndView;
+      }
 
       userDao.saveActualToken(user);
-      return new RedirectView("/login/token");
+      modelAndView.addObject("token", userDao.getActualToken());
+      modelAndView.setViewName("loginSuccess");
+
+      return modelAndView;
+   }
+
+   @GetMapping("/test")
+   public ModelAndView testNormalPage() {
+      ModelAndView modelAndView = new ModelAndView();
+      modelAndView.setViewName("index");
+
+      return modelAndView;
    }
 }
--- src/main/java/com/mkrasikoff/figmacomparator/models/User.java
+++ src/main/java/com/mkrasikoff/figmacomparator/models/User.java
@@ -2,8 +2,13 @@ package com.mkrasikoff.figmacomparator.models;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class User {
 
+   @NotEmpty(message = "Token shouldn't be empty")
+   @Size(min = 43, max = 43, message = "Please sure that you write a valid token")
    private String token;
 }
